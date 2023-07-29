e3a07626adbdb688461266307658e12c38e7777e
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-07-29 17:40:09 -0300

    pegando elementos por meio do evento submit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('novoItem')
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    console.log(e)
+    console.log(e.target.elements['nome'].value)
+    console.log(e.target.elements['quantidade'].value)
+})
