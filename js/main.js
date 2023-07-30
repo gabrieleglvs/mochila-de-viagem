a4ebc437b8dcfe365ef415b7bd477437d6d1db71
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-07-30 11:56:21 -0300

    refatorando o código e imprimindo itens do lS na tela

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const itens = JSON.parse(localStorage.getItem('itens')) || []
 
 /*PEGANDO CADA ELEMENTO ARMAZENADO NO LOCALSTORAGE PARA RECARREGÁ-LOS NA PAG*/
 itens.forEach((elemento) => {
-    console.log(elemento.nome, elemento.quantidade)
+    criaElemento(elemento) //agora só preciso chamar a função para que o que está no lS apareça na tela.
 })
 
 /*PASSANDO OS DADOS PARA A FUNÇÃO criaElemento*/
@@ -16,38 +16,37 @@ form.addEventListener("submit", (e) => {
 
     const nome = e.target.elements['nome']
     const quantidade = e.target.elements['quantidade']
+    
+    /*CRIANDO O OBJ*/
+    const itemAtual = {
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    }
 
-    criaElemento(nome.value, quantidade.value)
+    /*CHAMANDO A FUNÇÃO*/
+    criaElemento(itemAtual) //ele deixa de passar os dois paramentros e passa o obj que já é o conjunto dos dois
+
+    /*ARMAZENANDO DADOS NO LOCALSTORAGE*/
+    itens.push(itemAtual)
+
+    localStorage.setItem('itens', JSON.stringify(itens))
 
     nome.value = ""
     quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
-    console.log(nome)
-    console.log(quantidade)
+function criaElemento(itemAtual) {
 
     /*CRIANDO ELEMENTOS VIA JS*/
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = itemAtual.quantidade
 
     novoItem.appendChild(numeroItem)
 
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += itemAtual.nome
 
     lista.appendChild(novoItem)
-
-    /*ARMAZENANDO DADOS NO LOCALSTORAGE*/
-    
-    const itemAtual = {
-        'nome': nome,
-        'quantidade': quantidade
-    }
-
-    itens.push(itemAtual)
-
-    localStorage.setItem('itens', JSON.stringify(itens))
 }
