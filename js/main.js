8b55263c62c12a49714935355b08984abcb01c86
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-07-30 13:12:26 -0300

    atualizando quantidade de itens existentes apenas no HTML

--- js/main.js
+++ js/main.js
@@ -16,18 +16,32 @@ form.addEventListener("submit", (e) => {
 
     const nome = e.target.elements['nome']
     const quantidade = e.target.elements['quantidade']
-    
+
+    /*VERIFICANDO PRIMEIRO SE AQUELE ITEM JÁ EXISTE*/
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
     /*CRIANDO O OBJ*/
     const itemAtual = {
         'nome': nome.value,
         'quantidade': quantidade.value
     }
+    
+    /*CRIANDO UM ID pro item*/
+    if (existe) {
+        itemAtual.id = existe.id
+        //se o id existe continua o mesmo
+        
+        atualizaElemento(itemAtual)
+    } else {
+        itemAtual.id = itens.length
+        //se nao, cria um novo a partir do tamanho no array
 
-    /*CHAMANDO A FUNÇÃO*/
-    criaElemento(itemAtual) //ele deixa de passar os dois paramentros e passa o obj que já é o conjunto dos dois
+        /*CHAMANDO A FUNÇÃO*/
+        criaElemento(itemAtual) //ele deixa de passar os dois paramentros e passa o obj que já é o conjunto dos dois
 
-    /*ARMAZENANDO DADOS NO LOCALSTORAGE*/
-    itens.push(itemAtual)
+        /*ARMAZENANDO DADOS NO LOCALSTORAGE*/
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem('itens', JSON.stringify(itens))
 
@@ -43,10 +57,17 @@ function criaElemento(itemAtual) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = itemAtual.quantidade
+    //colocando dinamicamente o id no html
+    numeroItem.dataset.id = itemAtual.id
 
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += itemAtual.nome
 
     lista.appendChild(novoItem)
+}
+
+function atualizaElemento(itemAtual) {
+    //Busco o strong pelo ID e atualizo a quantidade apenas no HTML (falta atualizar no lS)
+    document.querySelector("[data-id='"+itemAtual.id+"']").innerHTML = itemAtual.quantidade
 }
